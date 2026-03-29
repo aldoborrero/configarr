f9dad3f7a28e5a42f09d61dfc64b3eb2bcfe4001
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-03-29 23:26:38 +0200

    refactor: move source to root, use blueprint package.nix for default package

--- nix/package.nix
+++ nix/package.nix
@@ -1,2 +1,45 @@
-{ perSystem, ... }:
-perSystem.self.configarr
+{
+  pkgs,
+  perSystem,
+  ...
+}:
+with pkgs;
+
+python313.pkgs.buildPythonApplication rec {
+  pname = "configarr";
+  version = "2.0.0";
+  pyproject = true;
+
+  src = lib.fileset.toSource {
+    root = ../.;
+    fileset = lib.fileset.unions [
+      ../pyproject.toml
+      ../configarr
+    ];
+  };
+
+  build-system = with python313.pkgs; [
+    setuptools
+  ];
+
+  dependencies = with python313.pkgs; [
+    click
+    pydantic
+    pyyaml
+    requests
+    rich
+    perSystem.self.bazarr-py
+    perSystem.self.prowlarr-py
+    perSystem.self.radarr-py
+    perSystem.self.sonarr-py
+  ];
+
+  meta = with lib; {
+    description = "Configuration manager for Radarr, Sonarr, Prowlarr, and Bazarr";
+    homepage = "https://github.com/aldoborrero/configarr";
+    license = licenses.asl20;
+    maintainers = [];
+    mainProgram = "configarr";
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
